af1ed94fc7031ff9665ac6e54400dae794f71bf1
Ingrid Biederman <biederman.14@gmail.com>
2018-12-18 11:12:55 -0500

    adding components to code

---
+++ src/Headline.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Headline extends Component {
+    render () {
+        return <h1>Welcome to Headline Component</h1>
+    }
+}
+
+export default Headline
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import App from './App'
 
-ReactDOM.render(<h1>Hello, React!</h1>, document.querySelector('#root'))
+ReactDOM.render(<App/>, document.querySelector('#root'))
