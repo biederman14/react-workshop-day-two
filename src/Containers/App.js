6c207fb2b7c6f2639b2d46ac99ce04512d2be0a0
Ingrid Biederman <biederman.14@gmail.com>
2018-12-18 13:36:21 -0500

    add babel to app

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
- import Headline from '../Components/Headline/Headline'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
+  
+  increment = () => {
+    this.setState({count: this.state.count + 1 });
+  }
+
   render() {
     return (
-      <div>
-        <Headline isColor text={"Hello from props."} />
-        <Headline isColor={false} text={"Wuzzup from bops."} />
-        <Headline isColor text={"Ciao from whops."} />
-        <p>This is some basic content.</p>
-        <p>THis shouldn't be styled.</p>
-      </div>
+      <main>
+        {this.state.count}
+        <button onClick={this.increment}>
+          Increment
+        </button>
+      </main>
     );
   }
 }
-export default App;
+
+
+export default App
